f6a1f415b2e96ea1232a92f3c6e6e09043bdf22a
John Gruska <johngruska@gmail.com>
2018-10-27 10:56:40 -0400

    Reset logic seems to work

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var request = require('request');
-var SunCalc = require('suncalc');
-var timerDict = require('./timerDictionary.js');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var request = require("request");
+var SunCalc = require("suncalc");
+var timerDict = require("./timerDictionary.js");
 
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
 
 // This interface will trigger the given event if the given time/location is at "dusk"
 // Here "dusk" is defined as half an hour before the sunset
 // Example query: http://lab.grapeot.me/ifttt/dusk?key=MY_KEY&lat=51.5&lon=-0.1&event=lightson
-app.get('//dusk', function(req, res) {
+app.get("//dusk", function(req, res) {
     var key = req.query.key;
     var lat = parseFloat(req.query.lat);
     var lon = parseFloat(req.query.lon);
@@ -32,15 +32,21 @@ app.get('//dusk', function(req, res) {
     var sunsetTime = times.sunset;
     var triggered = false;
     if (sunsetTime - new Date() < 0.5 * 3600 * 1000) {
-        var url = 'https://maker.ifttt.com/trigger/' + event + '/with/key/' + key;
-        console.log('URL = ' + url);
+        var url =
+            "https://maker.ifttt.com/trigger/" + event + "/with/key/" + key;
+        console.log("URL = " + url);
         request.post(url);
         triggered = true;
     }
-    res.send('Request recorded. Sunset time = ' + sunsetTime + '. ' + (triggered ? 'Triggered. ' : 'Not triggered.'));
+    res.send(
+        "Request recorded. Sunset time = " +
+            sunsetTime +
+            ". " +
+            (triggered ? "Triggered. " : "Not triggered.")
+    );
 });
 
-app.use('//cancel', function(req, res) {
+app.use("//cancel", function(req, res) {
     var key = req.query.key;
     var event = req.query.event;
     if (key == undefined || event == undefined) {
@@ -48,23 +54,31 @@ app.use('//cancel', function(req, res) {
         return;
     }
     timerDict.removeAllTimers(key, event);
-    res.send("All future times for { key = " + key + ", event = " + event + " } are removed. ");
+    res.send(
+        "All future times for { key = " +
+            key +
+            ", event = " +
+            event +
+            " } are removed. "
+    );
 });
 
-app.use('//delay', function(req, res) {
+app.use("//delay", function(req, res) {
     // Parse the inputs
     var delay = parseFloat(req.query.t); // in minutes, now allowing float
     var key = req.query.key;
     var event = req.query.event;
-    var reset = req.query.reset == '1';
+    var reset = req.query.reset == "1";
     if (reset == undefined) {
         reset = false;
     }
     if (delay == undefined || key == undefined || event == undefined) {
-        res.send("Error: must specify t (for delay time) and key and event in the URL.");
+        res.send(
+            "Error: must specify t (for delay time) and key and event in the URL."
+        );
         return;
     }
-    
+
     // Fetch the values from the POST body
     var value1 = req.body.Value1;
     var value2 = req.body.Value2;
@@ -74,46 +88,65 @@ app.use('//delay', function(req, res) {
         value2: encodeURIComponent(value2),
         value3: encodeURIComponent(value3)
     };
-    console.log('body = ' + JSON.stringify(bodyToSend));
-    
+    console.log("body = " + JSON.stringify(bodyToSend));
+
     var timerId = setTimeout(function() {
-        var url = 'https://maker.ifttt.com/trigger/' + event + '/with/key/' + key;
-        console.log('URL = ' + url);
-        request.post(url, { form: bodyToSend }, function(error, response, body) { });
+        var url =
+            "https://maker.ifttt.com/trigger/" + event + "/with/key/" + key;
+        console.log("TIMER EXPIRED!");
+        console.log("URL = " + url);
+        request.post(url, { form: bodyToSend }, function(
+            error,
+            response,
+            body
+        ) {});
         timerDict.removeTimer(key, event, timerId);
     }, delay * 60 * 1000);
     // If reset is specified, cancel all existing timers
     if (reset) {
+        console.log("RESETTING TIMERS!");
         timerDict.removeAllTimers(key, event);
     }
     // Add the current timer to the timer dictionary
     timerDict.addTimer(key, event, timerId);
-    res.send('Request recorded. Delay = ' + delay + ' minutes, event = ' + event + ', key = ' + key + ', reset = ' + reset);
+    res.send(
+        "Request recorded. Delay = " +
+            delay +
+            " minutes, event = " +
+            event +
+            ", key = " +
+            key +
+            ", reset = " +
+            reset
+    );
 });
 
-app.use('/', function(req, res) {
-    res.status(200).json({"status": "ok", "msg": "You've reached the API root. Service is running." });
+app.use("/", function(req, res) {
+    res.status(200).json({
+        status: "ok",
+        msg: "You've reached the API root. Service is running."
+    });
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if (app.get("env") === "development") {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render("error", {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
@@ -121,7 +154,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     console.log(err.message);
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
         message: err.message,
         error: {}
     });
